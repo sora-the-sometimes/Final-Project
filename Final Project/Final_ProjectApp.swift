0e792c41998374d6719f9c9fb6cd71f24241a797
Sora <kaithemaido@gmail.com>
2022-12-19 09:24:23 -0600

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Sora on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
